9034d96cd01dd51d37c897bb74c3ebef38feef09
Konstantin (Kosta) Baumann <kosta-github@ks-baumann.de>
2015-05-30 13:26:26 +0200

    eesec-prowl simplified

--- eesec-prowl.es6.js
+++ eesec-prowl.es6.js
@@ -21,10 +21,9 @@ let config_prowl = require('./config_siri.js');
 // ];
 // module.exports = config_prowl;
 
-const max_log_count = 100;
 const check_interval = 15; // in seconds
 
-const alarm_get_logs = '/action/logsGet?max_count=' + max_log_count;
+const alarm_get_status = '/action/panelCondGet';
 
 const prowl_url = 'https://api.prowlapp.com/publicapi/add';
 
@@ -51,42 +50,35 @@ let do_request = (url, user, password, cb) => {
 let last_mode = undefined;
 
 let check_status = () => {
-	debug('checking status...');
-
 	do_request(
-		config_alarm.base_url + alarm_get_logs, config_alarm.user, config_alarm.password,
+		config_alarm.base_url + alarm_get_status, config_alarm.user, config_alarm.password,
 		(err, data) => {
 			if(err) { debug('***** error: %s', JSON.stringify(err)); return; }
 
-			for(var i in data.logrows) {
-				let row = data.logrows[i];
-				if(row.action === '{LOG_MSG_MODE_CHANGED}') {
-			        let mode = row.mode, prio = 0;
-			        switch(mode) {
-						case '{AREA_MODE_0}': mode = 'Alarmanlage aus';  prio = 2; break;
-						case '{AREA_MODE_1}': mode = 'Alarmanlage an';   prio = 1; break;
-						case '{AREA_MODE_2}': mode = 'Alarmanlage home'; prio = 1; break;
-			        }
-					if(last_mode && (last_mode !== mode)) {
-						let desc = 'mode changed from "' + last_mode + '" to "' + mode + '"';
-						debug(desc);
-
-						let prowl_params = {
-							apikey:      collect_apikeys(config_prowl),
-							priority:    prio,
-							url:         '',
-							application: 'EESec',
-							event:       mode,
-							description: desc
-						};
-
-						request.post(prowl_url, { form: prowl_params });
-					}
-					last_mode = mode;
-
-					break;
-			    }
+			let mode = data.updates.mode_a1;
+			switch(mode) {
+				case '{AREA_MODE_0}': mode = 'Alarmanlage aus';  break;
+				case '{AREA_MODE_1}': mode = 'Alarmanlage an';   break;
+				case '{AREA_MODE_2}': mode = 'Alarmanlage home'; break;
 			}
+			debug('status: %s', mode);
+
+			if(last_mode && (last_mode !== mode)) {
+				let desc = 'mode changed from "' + last_mode + '" to "' + mode + '"';
+				debug(desc);
+
+				let prowl_params = {
+					apikey:      collect_apikeys(config_prowl),
+					priority:    prio,
+					url:         '',
+					application: 'EESec',
+					event:       mode,
+					description: desc
+				};
+
+				request.post(prowl_url, { form: prowl_params });
+			}
+			last_mode = mode;
 		}
 	);
 };
